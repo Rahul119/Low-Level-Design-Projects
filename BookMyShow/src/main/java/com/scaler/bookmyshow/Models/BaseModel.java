224893acf1b6a884fb9e3d5478d3994cdfc5393b
Rahul119 <rahulrajput027@gmail.com>
2025-04-04 22:57:18 +0530

    Add files via upload

---
+++ BookMyShow/src/main/java/com/scaler/bookmyshow/Models/BaseModel.java
@@ -0,0 +1,27 @@
+package com.scaler.bookmyshow.Models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+}
